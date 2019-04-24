187bf0a93701b6dea785fd134371117767c6c427
Amy Leong <amyleongg@gmail.com>
2019-04-23 21:51:08 -0400

    updated move

--- move.cpp
+++ move.cpp
@@ -22,24 +22,18 @@ class move : public board {
 */
 
 
-
-
-
-
-
-
-
 //  string user_vertical_input;                
 // cin>>user_vertical_input;
 //  if (user_vertical_input == "w") {
+
 void move_up(int array [5][5]){
   int counter;
   int a,b,c;
 
-  for (int counter=0;counter<3;counter++) {
+  for (int counter=0;counter<3;counter++) {             //this cycles for 3 times to ensure that all the values are moved up
     for ( int column = 0; column < 4; column++) {      //loops through the columns
-      for (int row=0; row < 4; row++) {        //loops through the rows 
-        if (array [row][column]== 0) {
+      for (int row=0; row < 4; row++) {               //loops through the rows within each column
+        if (array [row][column]== 0) {               //if there is an empty spot then swap so that all the values gets "pushed" or moved up
           a= array[row][column];
           b=array[row+1][column];
           c=b;
@@ -47,7 +41,7 @@ void move_up(int array [5][5]){
           a=c;
           array[row][column]=a;
           array[row+1][column]=b;
-        if (row==3){
+        if (row==3){                               //if you are at the last row then set the value equal to 0 or blank 
           array[row][column]=0;
     //      array[row+1][column]=-1;
         }
@@ -70,11 +64,6 @@ void combine_up (int array [5][5]) {
   }
   return; 
 }
-
-
-
-
-  
 
 
 //  else if (user_vertical_input == "s") {
@@ -121,11 +110,6 @@ void combine_down (int array[5][5]) {
 }
 
 
-
-
-
-
-
 void move_left (int array [5][5]) {
   int a,b,c;
 
@@ -153,8 +137,6 @@ void move_left (int array [5][5]) {
 }
 
 void combine_left (int array [5][5]) {
-
-
   for(int row =0;row<4;row++) {
     for (int column =0;column<4;column++) {
       if (array[row][column] == array [row][column+1]) {
@@ -167,11 +149,6 @@ void combine_left (int array [5][5]) {
  
   return;
 }
-
-
-
-
-
 
 
 
